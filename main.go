28fa3af5f3af0a8c84b94bf3af2219747138c0c8
edybalasa <msg4edy@gmail.com>
2024-09-03 11:18:22 +0300

    avion cu motor ia-l si pe sorin in zbor

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+
+	broadcastAddr, err := net.ResolveUDPAddr("udp4", "255.255.255.255:25643")
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		os.Exit(1)
+	}
+
+	conn, err := net.DialUDP("udp4", nil, broadcastAddr)
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Error retreiving hostname: ", err)
+	}
+
+	message := []byte(hostname)
+
+	for true {
+		_, err = conn.Write(message)
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Broadcast byte sent!")
+		time.Sleep(5000 * time.Millisecond)
+	}
+}
