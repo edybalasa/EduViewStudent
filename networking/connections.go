cb5794695e36d0f49de86ec1584e9b73e7f23552
edybalasa <msg4edy@gmail.com>
2024-10-27 13:23:26 +0200

    Better connection handling

--- networking/connections.go
+++ networking/connections.go
@@ -11,7 +11,7 @@ type SignalSender struct {
 	hostname string
 }
 
-func (sg SignalSender) SendSignals() {
+func (sg SignalSender) PrepareSendService() {
 	broadcastAddr, err := net.ResolveUDPAddr("udp4", "255.255.255.255:25643")
 	if err != nil {
 		fmt.Println("Error resolving address:", err)
@@ -32,8 +32,31 @@ func (sg SignalSender) SendSignals() {
 
 	message := []byte(sg.hostname)
 
+	sg.runPairingService(conn, message)
+}
+
+func (sg SignalSender) PrepareReceivingService() net.Conn {
+	addr, err := net.ResolveUDPAddr("udp4", ":25643")
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		os.Exit(1)
+	}
+
+	conn, err := net.ListenUDP("udp4", addr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Listening for broadcast messages on port 25643...")
+
+	return conn
+}
+
+func (sg SignalSender) runPairingService(sendConn net.Conn, message []byte) {
 	for true {
-		_, err := conn.Write(message)
+		_, err := sendConn.Write(message)
 		if err != nil {
 			fmt.Println("Error sending message:", err)
 			os.Exit(1)
@@ -42,4 +65,8 @@ func (sg SignalSender) SendSignals() {
 		fmt.Println("Broadcast byte sent!")
 		time.Sleep(5000 * time.Millisecond)
 	}
+}
+
+func (sg SignalSender) SendSignals() {
+	sg.PrepareSendService()
 }
